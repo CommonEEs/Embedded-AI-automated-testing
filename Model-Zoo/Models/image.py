d407a945dcd38e2da8936b08661afaf1d0bc669a
coramirez <114445970+coramirez@users.noreply.github.com>
2022-10-12 15:06:30 -0600

    Create image.py

---
+++ Model-Zoo/Models/image.py
@@ -0,0 +1,11 @@
+from model_zoo import Model
+import tensorflow as tf
+
+
+class ImagePredictionModel(Model):
+
+    def inputs(self):
+        return tf.keras.Input(shape=(13))
+
+    def outputs(self, inputs):
+        return tf.keras.layers.Dense(1)(inputs)
